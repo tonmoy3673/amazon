8b5f633302eb49aa7f8bcfa814b201bb77029dfb
tonmoy <tonmoy3673@gmail.com>
2023-11-18 02:00:34 +0600

    fetch products data

---
+++ src/Components/Products/Products.jsx
@@ -0,0 +1,9 @@
+const Products = () => {
+  return (
+    <div>
+      <h2>Product List</h2>
+    </div>
+  );
+};
+
+export default Products;
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+import "./Shop.css";
+import { useEffect } from "react";
+
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
-    <div>
-      <h2>Shop Page</h2>
+    <div className="shop-container py-10">
+      <div>
+        <h2>Product Details: </h2>
+        {}
+      </div>
+      <h2>Cart Details</h2>
     </div>
   );
 };
