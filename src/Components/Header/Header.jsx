2c55b37e23ca55a35d4ab758b2eb808fbb581966
tonmoy <tonmoy3673@gmail.com>
2023-11-18 00:50:28 +0600

    set navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,29 @@
-import logo from "../../images/logo";
+import logo from "../../images/logo.png";
+import "./Header.css";
 const Header = () => {
   return (
-    <nav>
-      <img src={logo} alt="img" />
+    <nav className="header">
+      <div className="flex">
+        <img src={logo} className="lg:w-8 w-4" alt="img" />
+        <h2 className="ms-3 text-xl font-semibold text-green-500 band">
+          {" "}
+          ShopBD{" "}
+        </h2>
+      </div>
+      <div className="menu">
+        <a className="me-4" href="/shop">
+          Shop
+        </a>
+        <a className="me-4" href="/oder">
+          Order
+        </a>
+        <a className="me-4" href="/inventory">
+          Inventory
+        </a>
+        <a className="me-4" href="/login">
+          Login
+        </a>
+      </div>
     </nav>
   );
 };
