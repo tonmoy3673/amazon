3f2264a4333bfefa78885fd72f73e8db9b772eb3
tonmoy <tonmoy3673@gmail.com>
2023-11-18 03:09:09 +0600

    added button

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,8 +1,8 @@
 const Products = (product) => {
   console.log(product.product);
-  const { id, img, name, price, seller, stock } = product.product;
+  const { id, img, name, price, seller, stock, ratings } = product.product;
   return (
-    <div className="border-2 py-2 pt-4 rounded-md">
+    <div className="border-2 py-2 pt-4 rounded-md truncate">
       <img
         className="lg:w-42 mx-auto w-9/12 rounded"
         src={img}
@@ -14,8 +14,14 @@ const Products = (product) => {
       <h5>
         Price : <span className="font-semibold text-red-700">${price}</span>
       </h5>
-      <h5>Seller : {seller}</h5>
-      <h5>Stock : {stock}</h5>
+      <h5 className="text-base">Seller : {seller}</h5>
+      <h5 className="text-base">Rating : {ratings} stars</h5>
+      <h5 className="text-base">Stock : {stock}</h5>
+      <div className="py-3">
+        <button className=" bg-green-500 font-semibold hover:bg-orange-500 w-full lg:w-1/2  p-2 rounded-md">
+          Add to Cart
+        </button>
+      </div>
     </div>
   );
 };
