fa197446d56a89eada24fe76879a0b986ba22583
tonmoy <tonmoy3673@gmail.com>
2023-11-20 01:29:23 +0600

    added tax

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -7,14 +7,14 @@ const Cart = (myCart) => {
   }
 
   return (
-    <div>
+    <div className="text-start px-7">
       <h2 className="text-green-500 text-xl font-semibold py-2">
         Order Details
       </h2>
       <h4>Selected Items :{cart.length}</h4>
       <h4>Total Price : {total}</h4>
       <h4>Shipping Cost: </h4>
-      <h4>Total Price : {total}</h4>
+      <h4>Tax : </h4>
     </div>
   );
 };
