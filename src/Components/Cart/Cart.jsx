58e2ece4f093c7bd71232a4079e93d6f613c8302
tonmoy <tonmoy3673@gmail.com>
2023-11-21 00:53:53 +0600

    set total cost

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -8,15 +8,19 @@ const Cart = (myCart) => {
     shippingCost = shippingCost + product.shipping;
   }
 
+  const tax = (totalPrice * 7) / 100;
+  const totalCost = totalPrice + shippingCost + tax;
+
   return (
     <div className="text-start px-7">
       <h2 className="text-green-500 text-xl font-semibold py-2">
         Order Details
       </h2>
-      <h4>Selected Items :{cart.length}</h4>
-      <h4>Total Price : {totalPrice}</h4>
-      <h4>Shipping Cost: {shippingCost}</h4>
-      <h4>Tax : </h4>
+      <h4>Selected Items : {cart.length}</h4>
+      <h4>Total Price : ${totalPrice}</h4>
+      <h4>Shipping Cost: ${shippingCost}</h4>
+      <h4>Tax : ${tax}</h4>
+      <h2 className="text-lg font-semibold">Total Cost: ${totalCost}</h2>
     </div>
   );
 };
