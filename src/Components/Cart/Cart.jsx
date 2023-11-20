ac5a08730658e8d631ad5e81e94f44b0be685480
tonmoy <tonmoy3673@gmail.com>
2023-11-21 00:50:46 +0600

    set shipping cost

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,9 +1,11 @@
 const Cart = (myCart) => {
   console.log(myCart);
   const { cart } = myCart;
-  let total = 0;
+  let totalPrice = 0;
+  let shippingCost = 0;
   for (const product of cart) {
-    total = total + product.price;
+    totalPrice = totalPrice + product.price;
+    shippingCost = shippingCost + product.shipping;
   }
 
   return (
@@ -12,8 +14,8 @@ const Cart = (myCart) => {
         Order Details
       </h2>
       <h4>Selected Items :{cart.length}</h4>
-      <h4>Total Price : {total}</h4>
-      <h4>Shipping Cost: </h4>
+      <h4>Total Price : {totalPrice}</h4>
+      <h4>Shipping Cost: {shippingCost}</h4>
       <h4>Tax : </h4>
     </div>
   );
