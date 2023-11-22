69257b246fae76d2835fa5c667a2c32527c241a1
tonmoy <tonmoy3673@gmail.com>
2023-11-23 00:57:55 +0600

    added product cart

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -23,8 +23,13 @@ const Shop = () => {
 
   useEffect(() => {
     const storedCart = getShoppingCart();
-    console.log(storedCart);
-  }, []);
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+      const quantity = storedCart[id];
+      addedProduct.quantity = quantity;
+      console.log(addedProduct);
+    }
+  }, [products]);
 
   return (
     <div className="shop-container py-10">
