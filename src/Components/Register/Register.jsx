c125f5ca00bdee29c0aae792d090f537a13d6e41
tonmoy <tonmoy3673@gmail.com>
2024-01-08 16:59:59 +0600

    set register page

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,7 +1,37 @@
 const Register = () => {
   return (
-    <div>
-      <h2>This is Register</h2>
+    <div className="text-center">
+      <h2 className="py-5 text-2xl font-semibold">Please Register</h2>
+      <div>
+        <form>
+          <input
+            className="mb-3 border-2 p-1 rounded-md"
+            type="text"
+            name="name"
+            placeholder="Your Name"
+          />
+          <br />
+          <input
+            className="mb-3 border-2 p-1 rounded-md"
+            type="email"
+            name="email"
+            placeholder="Your Email"
+          />
+          <br />
+          <input
+            className="mb-3 border-2 p-1 rounded-md"
+            type="password"
+            name="password"
+            placeholder="Your Password"
+          />
+          <br />
+          <input
+            className="mb-3 border-2 p-1 rounded-md cursor-pointer"
+            type="submit"
+            value="submit"
+          />
+        </form>
+      </div>
     </div>
   );
 };
